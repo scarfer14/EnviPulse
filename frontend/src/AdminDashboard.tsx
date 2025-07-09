ea34686b2061a383cca2634a45344573cf189afb
scarfer14 <dbriancarlo@gmail.com>
2025-07-09 10:44:26 +0800

    initial navbar commit

--- frontend/src/AdminDashboard.tsx
+++ frontend/src/AdminDashboard.tsx
@@ -2,8 +2,16 @@ import "./admin.css";
 
 export default function AdminDashboard() {
   return (
-    <div className="view-container">
-      <h1>Private Dashboard</h1>
+    <div className="admin-wrapper">
+      <div className="view-container">
+        <h1>Private Dashboard</h1>
+      </div>
+      <div className="nav-bar">
+        <button className="nav-btn">Main</button>
+        <button className="nav-btn">Temp</button>
+        <button className="nav-btn">Visitors</button>
+        <button className="nav-btn-out">Logout</button>
+      </div>
     </div>
   );
 }
